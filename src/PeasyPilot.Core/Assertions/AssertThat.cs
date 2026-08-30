8c7b4dc30c73a505b533e9923be850b33695cd39
Houssine <nidboubkerhoussine@live.fr>
2026-08-30 19:20:40 +0200

    update read me

--- src/PeasyPilot.Core/Assertions/AssertThat.cs
+++ src/PeasyPilot.Core/Assertions/AssertThat.cs
@@ -79,18 +79,4 @@ public class AssertThat<T>
     }
 }
 
-/// <summary>
-/// Entry point for fluent assertions.
-/// </summary>
-public static class Assert
-{
-    /// <summary>
-    /// Begins a fluent assertion on the given value.
-    /// </summary>
-    /// <typeparam name="T">The type of the value.</typeparam>
-    /// <param name="actual">The actual value.</param>
-    /// <param name="message">Optional assertion message.</param>
-    /// <returns>A new assertion builder.</returns>
-    public static AssertThat<T> That<T>(T actual, string? message = null)
-        => new(actual, message);
-}
+
